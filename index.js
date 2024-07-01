d896127277e6a6f8fd3bb70fd313b122487ec6bc
Atoti2 <bogyikormi3@gmail.com>
2024-07-01 16:07:58 +0200

    crud operations

--- index.js
+++ index.js
@@ -29,6 +29,54 @@ app.get('/api/persons', (request, response) => {
     response.json(people)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = people.find(p => p.id === id)
+
+    if(person){
+        response.json(person)
+    }else{
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    people.filter(p => p.id !== id)
+    response.status(204).end()    
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+            error: 'name or number is missing' 
+        })
+    }
+    const isAlreadyAdded = people.find((person) => person.name === body.name)
+
+    if(!isAlreadyAdded){
+        const person = {
+            id: Math.floor(Math.random() * 1000000) + "",
+            name: body.name,
+            number: body.number
+        }
+        people = people.concat(person)
+        response.json(person)
+    }else{
+        return response.status(400).json({ 
+            error: 'name is already added' 
+        })
+         
+    }
+
+    
+})
+
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${people.length} people</p><br/> ${new Date().toLocaleTimeString()}`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => `App is listening on port ${PORT}`)
 
