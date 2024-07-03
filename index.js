073f52d89a3e7d528215d959ad3639ce3b2a4495
Atoti2 <bogyikormi3@gmail.com>
2024-07-03 11:17:55 +0200

    mongodb atlas connection

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
@@ -10,6 +11,79 @@ app.use(morgan(':method :url - :total-time ms :body '))
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
+
+
+const Note = require('./models/note')
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)  
+    const note = notes.find(note => note.id === id)
+    if (note) {    
+        response.json(note)  
+    } else {    
+        response.status(404).end() 
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
+})
+
+app.post('/api/notes', (request, response) => { 
+    const body = request.body
+    if (!body.content) {
+        return response.status(400).json({ 
+          error: 'content missing' 
+        })
+      }
+
+    const note = new Note({
+        content: body.content,
+        important: Boolean(body.important) || false,
+    })
+
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
+})
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
 
 
 // let people = [
@@ -101,79 +175,3 @@ app.use(express.static('dist'))
 // const app = express()
 
 // app.use(express.json())
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)  
-    const note = notes.find(note => note.id === id)
-    if (note) {    
-        response.json(note)  
-    } else {    
-        response.status(404).end() 
-    }
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
-})
-
-const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => Number(n.id)))
-      : 0
-    return String(maxId + 1)
-  }
-  
-
-app.post('/api/notes', (request, response) => { 
-    const body = request.body
-    if (!body.content) {
-        return response.status(400).json({ 
-          error: 'content missing' 
-        })
-      }
-
-    const note = {
-        content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateId(),
-    }
-
-    notes = notes.concat(note)
-    response.json(note)
-})
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
-})
-
-
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
