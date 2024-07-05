b6b8f22a45e2d37d8e913806cbe6b2d2ab9d39d7
Atoti2 <bogyikormi3@gmail.com>
2024-07-05 10:25:30 +0200

    project structure changes

---
+++ app.js
@@ -0,0 +1,35 @@
+const notesRouter = require('./controllers/notes')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const middleware = require('./utils/middleware')
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require('mongoose')
+
+morgan.token("body", (req) => {
+    return JSON.stringify(req.body);
+  });
+  
+mongoose.set('strictQuery', false)
+logger.info('connecting to', config.MONGODB_URL)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
+
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(morgan(":method :url - :total-time ms :body "));
+app.use(cors());
+
+app.use('/api/notes', notesRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,116 +1,30 @@
-require("dotenv").config();
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const cors = require('cors')
-morgan.token('body', req => {
-    return JSON.stringify(req.body)
+const app = require('./app') // the actual Express application
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {    
-    return response.status(400).json({ error: error.message })
-  }
-  next(error)
-}
-
-app.use(express.static('dist'))
-app.use(express.json())
-app.use(morgan(':method :url - :total-time ms :body '))
-app.use(cors())
-
-
-const Note = require('./models/note')
-
-app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
-})
-
-app.delete('/api/notes/:id', (request, response, next) => {
-    Note.findByIdAndDelete(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    }).catch(error => next(error))
-})
-
-app.put('/api/notes/:id', (request, response, next) => {
-  const {content, important} = request.body
-
-  Note.findByIdAndUpdate(request.params.id,
-     { content, important },
-     { new: true, runValidators: true, context: "query" })
-  .then(updatedNote => response.json(updatedNote))
-  .catch(error => next(error))
-})
-
-app.post('/api/notes', (request, response, next) => { 
-    const body = request.body
-    const note = new Note({
-        content: body.content,
-        important: Boolean(body.important) || false,
-    })
-
-    note.save().then(savedNote => {
-      response.json(savedNote)
-    }).catch(error => next(error))
-})
-
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
-  
-app.use(unknownEndpoint)
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
-
-app.use(errorHandler)
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
-
-
 
 // let people = [
-//     { 
+//     {
 //       "id": "1",
-//       "name": "Arto Hellas", 
+//       "name": "Arto Hellas",
 //       "number": "040-123456"
 //     },
-//     { 
+//     {
 //       "id": "2",
-//       "name": "Ada Lovelace", 
+//       "name": "Ada Lovelace",
 //       "number": "39-44-5323523"
 //     },
-//     { 
+//     {
 //       "id": "3",
-//       "name": "Dan Abramov", 
+//       "name": "Dan Abramov",
 //       "number": "12-43-234345"
 //     },
-//     { 
+//     {
 //       "id": "4",
-//       "name": "Mary Poppendieck", 
+//       "name": "Mary Poppendieck",
 //       "number": "39-23-6423122"
 //     }
 // ]
@@ -133,14 +47,14 @@ app.listen(PORT, () => {
 // app.delete('/api/persons/:id', (request, response) => {
 //     const id = request.params.id
 //     people.filter(p => p.id !== id)
-//     response.status(204).end()    
+//     response.status(204).end()
 // })
 
 // app.post('/api/persons', (request, response) => {
 //     const body = request.body
 //     if (!body.name || !body.number) {
-//         return response.status(400).json({ 
-//             error: 'name or number is missing' 
+//         return response.status(400).json({
+//             error: 'name or number is missing'
 //         })
 //     }
 //     const isAlreadyAdded = people.find((person) => person.name === body.name)
@@ -154,13 +68,12 @@ app.listen(PORT, () => {
 //         people = people.concat(person)
 //         response.json(person)
 //     }else{
-//         return response.status(400).json({ 
-//             error: 'name is already added' 
+//         return response.status(400).json({
+//             error: 'name is already added'
 //         })
-         
+
 //     }
 
-    
 // })
 
 // app.get('/info', (request, response) => {
@@ -170,12 +83,11 @@ app.listen(PORT, () => {
 // const unknownEndpoint = (request, response) => {
 //     response.status(404).send({ error: 'unknown endpoint' })
 // }
-  
+
 // app.use(unknownEndpoint)
 
 // const PORT = 3001
 // app.listen(PORT, () => console.log(`App is listening on port ${PORT}`))
-
 
 // const express = require('express')
 // const app = express()
